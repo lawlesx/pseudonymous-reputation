67e09cf4ded6951f881acb7112a24a3a7c39c412
Aniruddha Sil <welcometohellv@gmail.com>
2023-02-12 16:12:36 +0530

    Render appropriate score using floor
    Random score generation

--- pages/chainbreakdown.js
+++ pages/chainbreakdown.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
 
 const Chainbreakdown = () => {
+
+  const [score, setScore] = useState(0)
+
+  useEffect(() => {
+    setScore(Math.floor((Math.random() * 1000) + 1))
+  },[])
+
   return (
     <div className='w-full h-full flex flex-col items-center justify-center px-20'>
       <h1 className='text-6xl flex flex-col items-center font-bold text-eggshell tracking-widest mt-12 gap-6'>
@@ -10,7 +17,7 @@ const Chainbreakdown = () => {
       </h1>
       <div className='w-full p-20 flex gap-4 justify-center'>
         <div className='relative w-[20rem] h-[25rem] bg-eggshell rounded-3xl'>
-          <h1 className='text-[7rem] h-full w-full flex items-center justify-center -rotate-45 font-semibold font-rubik text-space-cadet'>380</h1>
+          <h1 className='text-[7rem] h-full w-full flex items-center justify-center -rotate-45 font-semibold font-rubik text-space-cadet'>{score}</h1>
         </div>
         <div className='grid grid-cols-2 gap-4'>
           <div className='h-full w-[20rem] bg-eggshell rounded-3xl flex items-center justify-between'>
